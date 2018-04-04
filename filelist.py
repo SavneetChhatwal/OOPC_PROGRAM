890e76f8704bc38bb2874353d898b86142ce9ba1
savneetchhatwal <savneetchhatwal@gmail.com>
2018-04-05 00:00:00 +0530

    filelist.py added

--- filelist.py
+++ filelist.py
@@ -12,8 +12,8 @@ for (dirpath, dirnames, filenames) in os.walk('/home/krushn/Station/savneet'):
 
 
 for file in f:
-    d = str(random.randint(7,12)).zfill(2)+str(random.randint(1,30)).zfill(2)
-    os.system('sudo date +%Y%m%d -s "2017'+ d +'"')
+    d = str(random.randint(1,5)).zfill(2)+str(random.randint(1,30)).zfill(2)
+    os.system('sudo date +%Y%m%d -s "2018'+ d +'"')
     
     
     os.system("git add "+ file)
